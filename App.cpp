5bffd5164e73771f46a7198c33211e9018520a33
clavesi <34937562+clavesi@users.noreply.github.com>
2026-03-20 17:42:03 -0500

    Physical devices and queue families

--- App.cpp
+++ App.cpp
@@ -47,6 +47,12 @@ private:
     // Set up callback function for debug messenging
     vk::raii::DebugUtilsMessengerEXT debugMessenger = nullptr;
 
+    // Store graphics card
+    vk::raii::PhysicalDevice physicalDevice = nullptr;
+    std::vector<const char *> requiredDeviceExtension = {
+        vk::KHRSwapchainExtensionName
+    };
+
     void initWindow() {
         glfwInit();
 
@@ -60,6 +66,8 @@ private:
         // Create instance, the connection between the app and the Vulkan lib
         createInstance();
         setupDebugMessenger();
+        // Pick graphics card(s)
+        pickPhysicalDevice();
     }
 
     void createInstance() {
@@ -187,6 +195,55 @@ private:
         std::cerr << "validation layer: type " << to_string(type) << " msg: " << pCallbackData->pMessage << std::endl;
 
         return vk::False;
+    }
+
+    bool isDeviceSuitable(vk::raii::PhysicalDevice const &physicalDevice) {
+        // Check if the physicalDevice supports the Vulkan 1.3 API version
+        bool supportsVulkan1_3 = physicalDevice.getProperties().apiVersion >= vk::ApiVersion13;
+
+        // Check if any of the queue families support graphics operations
+        auto queueFamilies = physicalDevice.getQueueFamilyProperties();
+        bool supportsGraphics = std::ranges::any_of(queueFamilies, [](auto const &qfp) {
+            return !!(qfp.queueFlags & vk::QueueFlagBits::eGraphics);
+        });
+
+        // Check if all required physicalDevice extensions are available
+        auto availableDeviceExtensions = physicalDevice.enumerateDeviceExtensionProperties();
+        bool supportsAllRequiredExtensions =
+                std::ranges::all_of(requiredDeviceExtension,
+                                    [&availableDeviceExtensions](auto const &requiredDeviceExtension) {
+                                        return std::ranges::any_of(availableDeviceExtensions,
+                                                                   [requiredDeviceExtension](
+                                                               auto const &availableDeviceExtension) {
+                                                                       return strcmp(
+                                                                                  availableDeviceExtension.
+                                                                                  extensionName,
+                                                                                  requiredDeviceExtension) == 0;
+                                                                   });
+                                    });
+
+        // Check if the physicalDevice supports the required features (dynamic rendering and extended dynamic state)
+        auto features =
+                physicalDevice
+                .template getFeatures2<vk::PhysicalDeviceFeatures2, vk::PhysicalDeviceVulkan13Features,
+                    vk::PhysicalDeviceExtendedDynamicStateFeaturesEXT>();
+        bool supportsRequiredFeatures = features.template get<vk::PhysicalDeviceVulkan13Features>().dynamicRendering &&
+                                        features.template get<vk::PhysicalDeviceExtendedDynamicStateFeaturesEXT>().
+                                        extendedDynamicState;
+
+        // Return true if the physicalDevice meets all the criteria
+        return supportsVulkan1_3 && supportsGraphics && supportsAllRequiredExtensions && supportsRequiredFeatures;
+    }
+
+    void pickPhysicalDevice() {
+        std::vector<vk::raii::PhysicalDevice> physicalDevices = instance.enumeratePhysicalDevices();
+        auto const devIter = std::ranges::find_if(physicalDevices, [&](auto const &physicalDevice) {
+            return isDeviceSuitable(physicalDevice);
+        });
+        if (devIter == physicalDevices.end()) {
+            throw std::runtime_error("failed to find a suitable GPU!");
+        }
+        physicalDevice = *devIter;
     }
 };
 
