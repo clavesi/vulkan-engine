7b8cf6f5d8793856fadc2967593ed31603e4b037
clavesi <34937562+clavesi@users.noreply.github.com>
2026-05-06 04:01:57 -0500

    change: rename slang to hlsl - everything should still work, just has better clion support

--- src/core/UniformBufferObject.h
+++ src/core/UniformBufferObject.h
@@ -9,7 +9,7 @@
 #include <glm/glm.hpp>
 
 // Per-frame transformation data passed to the vertex shader.
-// Layout must match the UniformBuffer struct in shader.slang.
+// Layout must match the UniformBuffer struct in shader.hlsl.
 struct UniformBufferObject {
     glm::mat4 view;
     glm::mat4 proj;
